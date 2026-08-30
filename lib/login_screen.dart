aee8cdac8142af60ccffa8c5e91847fa43e77a2b
murlidharsoni173-ship-it <murlidharsoni173@gmail.com>
2026-08-30 12:47:20 +0530

    Create login_screen.dart

---
+++ lib/login_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart'; // File import karein
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(), // Yahan login screen call hogi
+    );
+  }
+}
