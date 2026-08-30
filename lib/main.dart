07b1f0b8a004d6fd105ac7fb9e8e4cebab9099f2
murlidharsoni173-ship-it <murlidharsoni173@gmail.com>
2026-08-30 13:46:20 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Firebase initialize ho raha hai
   runApp(const MyApp());
 }
 
